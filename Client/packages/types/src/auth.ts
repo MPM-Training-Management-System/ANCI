f52e0816572db16ad5ff910fcdd8177d738b398a
Stephen01623 <ralphjoedg@gmail.com>
2026-07-06 23:54:55 +0800

    feature:Databse Install

--- Client/packages/types/src/auth.ts
+++ Client/packages/types/src/auth.ts
@@ -5,6 +5,7 @@ export interface LoginRequest {
 
 export interface User {
   id: string;
+  username: string;
   fullName: string;
   email: string;
   role: string;
--- server/Program.cs
+++ server/Program.cs
@@ -1,10 +1,14 @@
 using server.Services;
-
+using Microsoft.EntityFrameworkCore;
+using server.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(
+        builder.Configuration.GetConnectionString("DefaultConnection")
+    ));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
